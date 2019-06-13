33f9f0629aea0bbca2493d10a3444b56e8e01abd
Firehiwot1 <fmazengia@gmail.com>
2019-06-12 23:28:30 -0400

    Math commit

---
+++ MathTwo/src/MathCalculation.java
@@ -0,0 +1,7 @@
+public class MathCalculation {
+    public static void main(String[] args) {
+        int mynumber = 2;
+        double myothernumber = 1.7938;
+        System.out.println("mynumber "+ mynumber + " and " + "myothernumber " + myothernumber );
+    }
+}
